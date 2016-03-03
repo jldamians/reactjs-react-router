e433f4a1bb206a7e7e4e0fd7fa3827b57a2c7c37
saavedrajl <jlds161089@gmail.com>
2016-03-03 15:26:24 -0500

    Configurando URLs amigables con la dependencia history

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,28 @@
 'use strict';
 
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
+/*
+ * hashHistory: si seteamos "history={hashHistory}", nuestras rutas
+ * por ejemplo: /, /developer, /backend, /frontend
+ * cada vez que accedamos a ellas tendremos un hash diferente
+ * /?_k=1ygitw, /developer?_k=7txep7
+ * estas url quedaran registrados en el historial
+ */
+
+//import { Router, Route, hashHistory } from 'react-router'
+
+/*
+ * Para quitar los hash de las url's ,
+ * implementamos 'history', que nos permite indicar 
+ * si las url se mostraran sin el hash que 'react-router' pone por defecto en las url's
+ * { queryKey: false }
+ * Entonces de esto '/developer?_k=7txep7' pasaremos a esto '/developers'
+ */
+
+import { Router, Route, useRouterHistory } from 'react-router'
+import { createHashHistory } from 'history'
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 import Welcome from '../components/welcome'
 import Backends from '../components/backends'
@@ -13,7 +34,7 @@ import Developers from '../components/developers'
     // http://rockyj.in/2015/12/31/react_routers_comparison.html
 
 let routes = (
-    <Router history={hashHistory}>
+    <Router history={appHistory}>
         <Route path='/' component={Welcome} />
         <Route path='developer' component={Developers} />
         <Route path='backend' component={Backends} />
